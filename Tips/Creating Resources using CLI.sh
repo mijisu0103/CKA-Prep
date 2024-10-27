e3ef275806e8be2150d409c1051fb19cc9638169
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-27 16:12:01 +0000

    Creating Resources using CLI.sh

---
+++ Tips/Creating Resources using CLI.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create an NGINX Pod
+kubectl run nginx --image=nginx
+
+# Generate POD Manifest YAML file (-o yaml). Do not create it (—dry-run)
+kubectl run nginx --image=nginx --dry-run=client -o yaml
+
+# Create a deployment
+kubectl create deployment --image=nginx nginx
+
+# Generate Deployment YAML file (-o yaml). Don’t create it (—dry-run)
+kubectl create deployment --image=nginx nginx --dry-run=client -o yaml
+
+# Generate Deployment YAML file (-o yaml). Don’t create it (—dry-run) and save it to a file
+kubectl create deployment --image=nginx nginx --dry-run=client -o yaml > nginx-deployment.yaml
+
+# Make necessary changes to the file  
+  # e.g. adding more replicas and then create the deployment
+kubectl create -f nginx-deployment.yaml
+
+# OR
+# In k8s version 1.19+, one can specify the --replicas option to create a deployment with 4 replicas
+kubectl create deployment --image=nginx nginx --replicas=4 --dry-run=client -o yaml > nginx-deployment.yaml
+
