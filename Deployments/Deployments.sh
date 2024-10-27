55f3edb0f7782d87f51ba9d89f7eaef61b7b58c7
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-27 17:37:44 +0000

    Deployments.sh

--- Deployments/Deployments.sh
+++ Deployments/Deployments.sh
@@ -7,25 +7,25 @@ deployment-definition.yml
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-	name: myapp-deployment
-	labels:
-		app: myapp
-		type: front-end
+  name: myapp-deployment
+  labels:
+    app: myapp
+    type: front-end
 spec:
-	template:
-		metadata:
-			name: myapp-pod
-			labels:
-				app: myapp
-				type: front-end
-			spec:
-				containers:
-				- name: nginx-container
-					image: nginx
-	replicas: 3
-	selector:
-		matchLabels:
-			type: front-end
+  template:
+    metadata:
+      name: myapp-pod
+      labels:
+        app: myapp
+        type: front-end
+      spec:
+        containers:
+        - name: nginx-container
+          image: nginx
+  replicas: 3
+  selector:
+    matchLabels:
+      type: front-end
 """
 
 # Once the file is ready
