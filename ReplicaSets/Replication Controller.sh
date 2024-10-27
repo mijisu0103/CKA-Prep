107a61f8e0a12554b6046e79cfb891643f01b302
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-27 01:41:31 +0100

    Replication Controller.sh

---
+++ ReplicaSets/Replication Controller.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# How to create a replication controller
+# Create rc-definition.yml
+  """
+  apiVersion: v1
+  kind: ReplicationControlelr
+  metadata:
+	  name: myapp-rc
+	  labels:
+		  app: myapp
+		  type: front-end
+  spec:
+	  template:
+		  metadata:
+			  name: myapp-pod
+			  labels:
+				  app: myapp
+			  	type: front-end
+		spec:
+			containers:
+			- name: nginx-controller
+				image: nginx
+				
+		replicas: 3
+  """
+
+# Once the file is ready, run the kubectl create command
+kubectl create -f rc-definition.yml
+
+# To view the list of created replication controllers:
+kubectl get replicationcontroller
+
+# To see the pods created by the replication controller
+kubectl get pods
